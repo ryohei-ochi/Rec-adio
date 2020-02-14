b6ba0b6cd4a6667df360e94549106752544c1ce3
Ryohei Ochi <ryohei.ochi@gmail.com>
2020-02-14 13:54:46 +0900

    tablesorter周りと、urlencodeのバグ修正

--- public_html/index.php
+++ public_html/index.php
@@ -28,7 +28,7 @@ try {
     if(isset($_GET["id"])) {
         $stmt = $dbh->prepare("UPDATE Programs SET `count` = `count` + 1 WHERE id = :id");
         $stmt->execute(array(":id" => $_GET["id"]));
-        header('Location: ' . $_GET["url"], true, 301);
+        header('Location: ' . $_GET["url1"]."/".urlencode($_GET["url2"])."/".urlencode($_GET["url3"]) , true, 301);
     }
 } catch (PDOException $e) {
     echo $e->getMessage();
@@ -64,7 +64,7 @@ window.onpageshow = function(event) {
 $(function() {
 
     $("#groups").tablesorter({
-    //sortList: [[0,0],[2,0]],
+    sortList: [[0,0],[3,1]],
     theme : "blue",
     headers: {
         5: { sorter: false }
@@ -74,46 +74,13 @@ $(function() {
     widgetOptions: {
         group_collapsible : true,  // make the group header clickable and collapse the rows below it.
         group_collapsed   : true, // start with all groups collapsed (if true)
-        group_saveGroups  : true,  // remember collapsed groups
+        group_saveGroups  : false,  // remember collapsed groups
         group_saveReset   : '.group_reset', // element to clear saved collapsed groups
         group_count       : " ({num})", // if not false, the "{num}" string is replaced with the number of rows in the group
 
         // apply the grouping widget only to selected column
-        group_forceColumn : [],   // only the first value is used; set as an array for future expansion
+        group_forceColumn : [0],   // only the first value is used; set as an array for future expansion
         group_enforceSort : true, // only apply group_forceColumn when a sort is applied to the table
-
-        // checkbox parser text used for checked/unchecked values
-        group_checkbox    : [ 'checked', 'unchecked' ],
-
-        // change these default date names based on your language preferences (see Globalize section for details)
-        group_months      : [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
-        group_week        : [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
-        group_time        : [ "AM", "PM" ],
-
-        // use 12 vs 24 hour time
-        group_time24Hour  : false,
-        // group header text added for invalid dates
-        group_dateInvalid : 'Invalid Date',
-
-        // this function is used when "group-date" is set to create the date string
-        // you can just return date, date.toLocaleString(), date.toLocaleDateString() or d.toLocaleTimeString()
-        // reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#Conversion_getter
-        group_dateString  : function(date) {
-        return date.toLocaleString();
-        },
-
-        group_formatter   : function(txt, col, table, c, wo, data) {
-        // txt = current text; col = current column
-        // table = current table (DOM); c = table.config; wo = table.config.widgetOptions
-        // data = group data including both group & row data
-        if (col === 7 && txt.indexOf("GMT") > 0) {
-            // remove "GMT-0000 (Xxxx Standard Time)" from the end of the full date
-            // this code is needed if group_dateString returns date.toString(); (not localeString)
-            txt = txt.substring(0, txt.indexOf("GMT"));
-        }
-        // If there are empty cells, name the group "Empty"
-        return txt === "" ? "Empty" : txt;
-        },
 
         group_callback    : function($cell, $rows, column, table) {
         // callback allowing modification of the group header labels
@@ -135,21 +102,8 @@ $(function() {
 });
 </script>
 <style type="text/css">
-body {background-color: #fff; color: #222; font-family: sans-serif;}
-pre {margin: 0; font-family: monospace;}
-a:link {color: #009; text-decoration: none; background-color: #fff;}
-a:hover {text-decoration: underline;}
-table {border-collapse: collapse; border: 0; box-shadow: 1px 2px 3px #ccc;}
-.center {text-align: center;}
-.center table {margin: 1em auto; text-align: left;}
-.center th {text-align: center !important;}
-td, th {border: 1px solid #666; font-size: 75%; vertical-align: baseline; padding: 4px 5px;}
-h1 {font-size: 150%;}
-h2 {font-size: 125%;}
-.t {font-weight: bold;}
-.f {font-weight: bold;}
-.d {font-weight: bold; text-align: center;}
-.v {font-weight: bold; text-align: center;}
+.t, .f {font-weight: bold;}
+.d, .b, .c, .s {font-weight: bold; text-align: center;}
 </style>
 <title>Listen - Rec-adio</title><meta name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE" /></head>
 <body>
@@ -160,8 +114,6 @@ h2 {font-size: 125%;}
             <tbody>
 <?php
 try {
-//    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD, $options);
-//    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "SELECT * FROM Programs WHERE path IS NOT NULL";
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
@@ -179,40 +131,13 @@ while($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
     ."<td class=\"d\">".date("Y/m/d H:i", strtotime($res["rec-timestamp"]))."</td>"
     ."<td class=\"c\">".$res["count"]."</td>"
     ."<td class=\"b\"><a href=\"index.php?id=".$res["id"]
-    ."&url="."./savefile/".urlencode($file[0])."/".urlencode($file[1])
+    ."&url1=./savefile"
+    ."&url2=".urlencode($file[0])
+    ."&url3=".urlencode($file[1])
     ."&tmp=".uniqid()."\">"
     ."<i class=\"material-icons\">play_circle_filled</i></td>"
     ."</a></tr>\n";
 }
-
-/*
-foreach(glob("/mnt/rec/*.log",GLOB_NOSORT) as $file){
-    if(is_file($file)){
-        $lines = file($file);
-        $drop = 0;
-        foreach ($lines as $line){
-            if(preg_match('/drop: (?P<drop>\d+)/', $line, $match)){
-                $drop = $drop + $match["drop"];
-            }
-        }
-        
-        if($drop != 0){
-            echo "<tr>"
-                ."<td class=\"t\">".date("Y/m/d H:i:s",filemtime($file))."</td>"
-                ."<td class=\"f\">".htmlspecialchars($file)."</td>"
-                ."<td class=\"d\">".$drop."</td>"
-                ."<td class=\"v\"><button class=\"btn\" value=\"".$file."\" disabled>view</button></td>"
-                ."</tr>\n";
-        }else{
-            unlink($file);
-        }
-        
-        @ob_flush();
-        @flush();
-        
-    }
-}
-*/
 ?>
             </tbody>
         </table>
